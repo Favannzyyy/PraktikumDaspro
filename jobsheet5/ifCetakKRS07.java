a3bc4683b6481e507d6a0c95441caf3676946fd7
Favannzyyy <favannzyyy@gmail.com>
2025-10-02 07:40:42 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS07.java
+++ jobsheet5/ifCetakKRS07.java
@@ -11,9 +11,10 @@ public class ifCetakKRS07 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
 
         sc.close();
     }
 }
-
